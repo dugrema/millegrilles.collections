0595499db5366ad39afb7f8ae85ca5ac58001f98
Mathieu Dugre <mathieu.dugre@mdugre.info>
2021-12-13 11:29:00 -0500

    Refact verif auth pour compte_prive

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import { configurerEvenements } from './appSocketIo.js'
 import routeCollections from './routes/collections.js'
 import * as mqdao from './mqdao.js'
 
-const debug = debugLib('millegrilles:app')
+const debug = debugLib('app')
 const { extraireExtensionsMillegrille } = utiljs.forgecommon
 
 export default async function app(params) {
@@ -40,25 +40,50 @@ export default async function app(params) {
 
 function verifierAutorisation(socket, securite, certificatForge) {
 
-    if(securite === '3.protege') {
-        const extensions = extraireExtensionsMillegrille(certificatForge)
-        debug("www.verifierAutorisation extensions %O", extensions)
+    let prive = false, protege = false
 
+    const extensions = extraireExtensionsMillegrille(certificatForge)
+    console.debug("!!! www.verifierAutorisation extensions %O", extensions)
+
+
+    if(['proprietaire', 'delegue'].includes(extensions.delegationGlobale)) {
         // Deleguation globale donne tous les acces
-        if(['proprietaire', 'delegue'].includes(extensions.delegationGlobale)) {
-            debug("Usager proprietaire, acces 3.protege OK")
-            return true
-        }
-
+        debug("Usager proprietaire, acces 3.protege OK")
+        prive = true
+        protege = true
+    } else if(extensions.delegationsDomaines.includes('collections')) {
         // Delegation au domaine coupdoeil
-        if(extensions.delegationsDomaines.includes('collections')) {
-            debug("Usager delegue domaine coupdoeil, acces 3.protege OK")
-            return true
+        debug("Usager delegue domaine coupdoeil, acces 3.protege OK")
+        prive = true
+        protege = true
+    } else if(securite === '2.prive') {
+        const roles = extensions.roles || []
+        if(roles.includes('compte_prive')) {
+            debug("Usager prive, acces 2.prive OK")
+            prive = true
         }
-
-        debug("Usager acces 3.protege refuse")
     }
     
-    return false
+    // if(securite === '3.protege') {
+    //     // Deleguation globale donne tous les acces
+    //     if(['proprietaire', 'delegue'].includes(extensions.delegationGlobale)) {
+    //         debug("Usager proprietaire, acces 3.protege OK")
+    //         prive = true
+    //         protege = true
+    //     }
+
+    //     // Delegation au domaine coupdoeil
+    //     if(extensions.delegationsDomaines.includes('collections')) {
+    //         debug("Usager delegue domaine coupdoeil, acces 3.protege OK")
+    //         prive = true
+    //         protege = true
+    //     }
+
+    //     debug("Usager acces 3.protege refuse")
+    // } else if(securite === '2.prive') {
+
+    // }
+    
+    return {prive, protege}
 }
 
