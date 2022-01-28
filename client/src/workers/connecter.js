c4e3a2505bb790f39e1dc9716c7479f7caee2a63
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-01-28 17:04:59 -0500

    Refact ed25519

--- client/src/workers/connecter.js
+++ client/src/workers/connecter.js
@@ -1,11 +1,11 @@
 import { proxy } from 'comlink'
 
-const CONST_APP_URL = 'collections'
+const CONST_APP_URL = 'collections/socket.io'
 
 export async function connecter(workers, setUsagerState, setEtatConnexion) {
     const { connexion } = workers
   
-    // console.debug("Set callbacks connexion worker")
+    console.debug("Set callbacks connexion worker")
     const location = new URL(window.location.href)
     location.pathname = CONST_APP_URL
     console.info("Connecter a %O", location.href)
@@ -21,11 +21,11 @@ export async function connecter(workers, setUsagerState, setEtatConnexion) {
 
 async function setUsager(workers, nomUsager, setUsagerState, opts) {
     opts = opts || {}
-    // console.debug("setUsager '%s'", nomUsager)
-    const { getUsager } = await import('@dugrema/millegrilles.reactjs')
-    const { forgecommon } = await import('@dugrema/millegrilles.utiljs')
-    const { pki } = await import('node-forge')
-    const { extraireExtensionsMillegrille } = forgecommon
+    console.debug("setUsager '%s'", nomUsager)
+    const { getUsager, utiljs } = await import('@dugrema/millegrilles.reactjs')
+    console.debug("utiljs : %O", utiljs)
+    const { pki } = await import('@dugrema/node-forge')
+    const { extraireExtensionsMillegrille } = utiljs.forgecommon
     const usager = await getUsager(nomUsager)
     // console.debug("Usager info : %O", usager)
     
@@ -41,8 +41,8 @@ async function setUsager(workers, nomUsager, setUsagerState, opts) {
         await x509.init(caPem)
 
         // Init cles privees
-        await chiffrage.initialiserFormatteurMessage(certificatPem, usager.dechiffrer, usager.signer, {DEBUG: true})
-        await connexion.initialiserFormatteurMessage(certificatPem, usager.signer, {DEBUG: false})
+        await chiffrage.initialiserFormatteurMessage(certificatPem, usager.clePriveePem, {DEBUG: true})
+        await connexion.initialiserFormatteurMessage(certificatPem, usager.clePriveePem, {DEBUG: false})
     
         const certForge = pki.certificateFromPem(fullchain[0])
         const extensions = extraireExtensionsMillegrille(certForge)
