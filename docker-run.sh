af9635265e8912e10887d7bd639bda5dad23049d
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-07-10 15:03:55 -0400

    lock

--- docker-run.sh
+++ docker-run.sh
@@ -13,10 +13,10 @@ echo Image docker : $IMAGE_DOCKER
 export HOST=mg-dev5.maple.maceroc.com
 export HOST_MQ=mg-dev5
 
-CERT_FOLDER=/home/mathieu/mgdev/certs
+CERT_FOLDER=/var/opt/millegrilles/secrets
 export MG_MQ_CAFILE=/certs/pki.millegrille.cert
 export MG_MQ_CERTFILE=/certs/pki.collections.cert
-export MG_MQ_KEYFILE=/certs/pki.collections.key
+export MG_MQ_KEYFILE=/certs/pki.collections.cke
 export MG_MQ_URL=amqps://$HOST_MQ:5673
 export MG_REDIS_HOST=mg-dev5
 export PORT=3037
@@ -26,7 +26,7 @@ export DEBUG=millegrilles:common:server4,millegrilles:maitrecomptes:authentifica
 
 docker run --rm -it \
   --network host \
-  -v /home/mathieu/mgdev/certs:/certs \
+  -v /var/opt/millegrilles/secrets:/certs \
   -e MG_MQ_CAFILE -e MG_MQ_CERTFILE -e MG_MQ_KEYFILE \
   -e MG_MQ_URL -e HOST -e PORT \
   -e MG_REDIS_HOST \
