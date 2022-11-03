4807fc83544fce12fee6d46f08bffb18ce8ae98e
Mathieu Dugre <mathieu.dugre@mdugre.info>
2022-11-02 21:20:55 -0400

    Session keepalive

--- client/src/WorkerContext.js
+++ client/src/WorkerContext.js
@@ -143,6 +143,8 @@ async function verifierSession() {
         const importAxios = await import('axios')
         const reponse = await importAxios.default.get('/millegrilles/authentification/verifier')
         console.debug("Reponse verifier session sur connexion : ", reponse)
+        const reponseCollections = await importAxios.default.get('/collections/initSession')
+        console.debug("Reponse verifier session sur collections : ", reponseCollections)
     } catch(err) {
         redirigerPortail(err)
     }
