c945c16aa4da96d38b0faedcadcbc25d38c43997
mathieu <mathieu.dugre@mdugre.info>
2023-10-04 12:32:22 -0400

    Ajout param reconnectionDelay 5s

--- client/src/workers/connecter.js
+++ client/src/workers/connecter.js
@@ -24,7 +24,7 @@ export async function connecter(workers, setUsagerState, setEtatConnexion, setEt
     //     console.error("Erreur init session : %O", err)
     // }
 
-    return connexion.connecter(location.href)
+    return connexion.connecter(location.href, {reconnectionDelay: 5_000})
 }
 
 async function setUsager(workers, nomUsager, setUsagerState, opts) {
