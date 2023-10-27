c2d5e3f0c95403183fa0288a0116c43b8f5fdf6a
mathieu <mathieu.dugre@mdugre.info>
2023-10-27 13:24:08 -0400

    Ajustement installation service worker

--- client/public/sw.js
+++ client/public/sw.js
@@ -5,32 +5,34 @@ const CACHE_NAME = 'collections-cache'
 self.addEventListener('install', e => {
     console.log('installing service worker')
 
-    fetch('/collections/asset-manifest.json')
-        .then(response => {
-            if(response.status !== 200) {
-                console.warn("Erreur installation assets : http status ", response.status)
-                return
-            }
-            
-            response.json()
-                .then(async manifest => {
-                    console.debug("Asset manifest : ", manifest)
-                    const fileKeys = Object.keys(manifest.files).filter(item=>{
-                        return ! ['index.html'].includes(item)
+    e.waitUntil(
+        fetch('/collections/asset-manifest.json')
+            .then(response => {
+                if(response.status !== 200) {
+                    console.warn("Erreur installation assets : http status ", response.status)
+                    return
+                }
+                
+                response.json()
+                    .then(async manifest => {
+                        console.debug("Asset manifest : ", manifest)
+                        const fileKeys = Object.keys(manifest.files).filter(item=>{
+                            return ! ['index.html'].includes(item)
+                        })
+                        const files = fileKeys.map(key=>manifest.files[key])
+
+                        // Cleanup du cache precedent
+                        await caches.delete(CACHE_NAME)
+
+                        console.debug("Cache all files ", files)
+                        const cache = await caches.open(CACHE_NAME)
+                        await cache.addAll(files)
+                        console.debug("Caching de %d fichiers reussi", files.length)
                     })
-                    const files = fileKeys.map(key=>manifest.files[key])
+                    .catch(err=>console.error("Erreur lecture asset-manifest.json ou caching assets ", err))
 
-                    // Cleanup du cache precedent
-                    await caches.delete(CACHE_NAME)
-
-                    console.debug("Cache all files ", files)
-                    const cache = await caches.open(CACHE_NAME)
-                    await cache.addAll(files)
-                    console.debug("Caching de %d fichiers reussi", files.length)
-                })
-                .catch(err=>console.error("Erreur lecture asset-manifest.json ou caching assets ", err))
-
-        })
+            })
+    )
 
     // e.waitUntil(
     //     caches.open(CACHE_NAME).then( cache => {
@@ -59,8 +61,6 @@ self.addEventListener('fetch', e => {
         return
     }
 
-    console.log(`fetching/caching ${e.request.url}`)
-
     const reponse = caches.match(e.request)
         .then( response => {
             if(response) {
@@ -69,6 +69,7 @@ self.addEventListener('fetch', e => {
             }
 
             if( navigator.onLine ) {
+                console.info(`fetching/caching ${e.request.url}`)
                 const fetchRequest = e.request.clone()
                 return fetch(fetchRequest).then( response => {
                     if (!response || response.status !== 200 || response.type !== 'basic' ) {
