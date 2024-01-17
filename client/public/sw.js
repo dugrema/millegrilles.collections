37c4810bc0db517640b0dc008e5a95f7693ef72c
mathieu <mathieu.dugre@mdugre.info>
2024-01-17 08:43:28 -0500

    Cleanup sw

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,9 +1,10 @@
 console.info('sw.js Self : ', self)
 
 const CACHE_NAME = 'collections-cache'
+const CONST_CACHE_DOWNLOADS = 'fichiersDownloadDechiffres'
 
 self.addEventListener('install', e => {
-    console.log('installing service worker')
+    console.log('installing service worker 2024.0.64')
 
     e.waitUntil(
         fetch('/collections/asset-manifest.json')
@@ -34,59 +35,39 @@ self.addEventListener('install', e => {
             })
     )
 
-    // e.waitUntil(
-    //     caches.open(CACHE_NAME).then( cache => {
-    //         return cache.addAll([
-    //             '/collections',
-    //             '/collections/index.html',
-    //             '/collections/static/js/bundle.js'
-    //         ])
-    //     })
-    //     .then(() => self.skipWaiting())
-    // )
 })
 
 self.addEventListener('activate', e => {
     console.log('activating service worker')
-    e.waitUntil(self.clients.claim())
+    //e.waitUntil(self.clients.claim())
 })
 
-const PATHNAMES_GET_SUPPORTES = ['static', 'streams']
+const METHODS_STREAM = ['GET', 'HEAD']
 
 self.addEventListener('fetch', e => {
-    const url = new URL(e.request.url)
-    console.info("fetch ", url)
     const method = e.request.method
+    const url = new URL(e.request.url)
+    // console.info("fetch %s url %s, destination %s", method, url, destination)
 
-    // Determiner si la path supporte le caching
-    const pathname = url.pathname
-    let subpath = null
-    if( ['HEAD', 'GET'].includes(method)) {
-        const pathSplit = pathname.split('/')
-        console.debug("path split ", pathSplit)
-        if(pathSplit.length > 2) {
-            subpath = pathSplit[2]
-            if(!PATHNAMES_GET_SUPPORTES.includes(subpath)) 
-            {
-                // pas de caching
-                return
-            }
-        }
-    } else {
-        // pas de caching
-        return
-    }
-
-    if(subpath === 'streams') {
+    if(METHODS_STREAM.includes(method) && url.pathname.startsWith('/collections/streams/')) {
         traiterCacheStream(e)
         return
     }
 
+    if(method !== 'GET') return
+    
+    if(url.pathname.startsWith('/collections/static/')) {
+        traiterCacheApp(e)
+        return
+    }
+})
+
+function traiterCacheApp(e) {
     // Path /collections/static
     const reponse = caches.match(e.request)
         .then( response => {
             if(response) {
-                console.debug("fetch utilise cache ", response)
+                // console.debug("fetch utilise cache ", response)
                 return response
             }
 
@@ -97,102 +78,100 @@ self.addEventListener('fetch', e => {
                     if (!response || response.status !== 200 || response.type !== 'basic' ) {
                         return response
                     }
-        
+
                     const responseToCache = response.clone()
-        
+
                     caches.open(CACHE_NAME)
                         .then( cache => {
                             cache.put(e.request, responseToCache)
                         })
-        
+
                     return response
                 })
             }
         })
-    e.respondWith(reponse)
-})
-
-const CONST_CACHE_DOWNLOADS = 'fichiersDownloadDechiffres'
+    e.respondWith(reponse)    
+}
 
 function traiterCacheStream(e) {
-    const url = new URL(e.request.url)
-    const method = e.request.method
-    console.debug("traiterCacheStream Request (method: %s) : %O ", method, e.request)
-    
-    const pathname = url.pathname
-    const pathsplit = pathname.split('/')
-    if(pathsplit.length <= 3) {
-        return  // Pas de fuuid
-    }
+    try {
+        const url = new URL(e.request.url)
+        const method = e.request.method
+        // console.debug("traiterCacheStream Request (method: %s) : %O ", method, e.request)
+        
+        const pathname = url.pathname
+        const pathsplit = pathname.split('/')
+        if(pathsplit.length <= 3) {
+            // console.debug("Pas de fuuid sur ", url.href)
+            return  // Pas de fuuid
+        }
 
-    const fuuid = pathsplit[3]
-    console.debug("traiterCacheStream fuuid %s", fuuid)
+        const fuuid = pathsplit[3]
+        console.debug("traiterCacheStream fuuid %s", fuuid)
 
-    const promise = caches.open(CONST_CACHE_DOWNLOADS)
-        .then( async cache => {
-            const pathFuuid0 = `/${fuuid}/0`
-            const elem0 = await cache.match(pathFuuid0)
-            if(elem0) {
-                // On va combiner tous les elemens de cache en un seul blob pour recreer le video complet
-                console.debug("On a trouver le video en cache : ", elem0)
+        const promise = caches.open(CONST_CACHE_DOWNLOADS)
+            .then( async cache => {
+                const pathFuuid0 = `/${fuuid}/0`
+                const elem0 = await cache.match(pathFuuid0)
+                if(elem0) {
+                    // On va combiner tous les elemens de cache en un seul blob pour recreer le video complet
+                    // console.debug("On a trouver le video en cache : ", elem0)
 
-                const parts = await getPartsDownload(fuuid)
-                const partsBlobs = []
-                for(const part of parts) {
-                    partsBlobs.push(await part.response.blob())
-                }
-
-                // Extraire information de posision
-                const headers = e.request.headers
-                const range = headers.get('Range')
-
-                const blobComplet = new Blob(partsBlobs)
-                const taille = blobComplet.size
-
-                let start = 0, end = taille - 1
-                if(range) {
-                    console.debug("Range stream : ", range)
-                    const paramsRange = range.split('=').pop()
-                    const splitRange = paramsRange.split('-')
-                    start = Number.parseInt(splitRange[0])
-                    if(splitRange.length > 1) {
-                        if(splitRange[1]) {
-                            end = Number.parseInt(splitRange[1])
-                        }
+                    if(method === 'HEAD') {
+                        // console.debug("Reponse HEAD")
+                        return new Response('', {status: 200})
                     }
-                    console.debug("Split start %d, end %d, splitRange %O", start, end, splitRange)
-                }
 
-                if(method === 'HEAD') {
-                    console.debug("Reponse HEAD")
+                    const parts = await getPartsDownload(fuuid)
+                    const partsBlobs = []
+                    for(const part of parts) {
+                        partsBlobs.push(await part.response.blob())
+                    }
+
+                    // Extraire information de posision
+                    const headers = e.request.headers
+                    const range = headers.get('Range')
+
+                    const blobComplet = new Blob(partsBlobs)
+                    const taille = blobComplet.size
+
+                    let start = 0, end = taille - 1
+                    if(range) {
+                        // console.debug("Range stream : ", range)
+                        const paramsRange = range.split('=').pop()
+                        const splitRange = paramsRange.split('-')
+                        start = Number.parseInt(splitRange[0])
+                        if(splitRange.length > 1) {
+                            if(splitRange[1]) {
+                                end = Number.parseInt(splitRange[1])
+                            }
+                        }
+                        // console.debug("Split start %d, end %d, splitRange %O", start, end, splitRange)
+                    }
+
+                    const contentRange = `bytes ${start}-${end}/${taille}`
+                    const blobSlice = blobComplet.slice(start, end+1)
+
                     const headersResponse = new Headers()
-                    headersResponse.set('Content-Length', ''+taille)
-                    return new Response('', {status: 200, headers: headersResponse})
+                    headersResponse.set('Content-Length', ''+(end - start + 1))
+                    headersResponse.set('Content-Range', contentRange)
+
+                    const responseCache = new Response(blobSlice, {status: 206, headers: headersResponse})
+                    return responseCache
                 }
-
-                const contentRange = `bytes ${start}-${end}/${taille}`
-                const blobSlice = blobComplet.slice(start, end+1)
-
-                const headersResponse = new Headers()
-                headersResponse.set('Content-Length', ''+(end - start + 1))
-                headersResponse.set('Content-Range', contentRange)
-
-                const responseCache = new Response(blobSlice, {status: 206, headers: headersResponse})
-                return responseCache
-            }
-            console.debug("Cache miss sur %s", fuuid)
-            return fetch(e.request).then(response=>{
-                console.debug("Response status ", response.status)
-                return response
+                
+                // console.debug("Cache miss sur %s, executer %s", fuuid, e.request.url)
+                return fetch(e.request)
             })
-        })
-        .catch(err=>{
-            console.error("Erreur traitement cache streaming, on fait le fetch demande", err)
-            return fetch(e.request)
-        })
+            .catch(err=>{
+                console.error("Erreur traitement cache streaming, on fait le fetch demande", err)
+                return fetch(e.request)
+            })
 
-    e.waitUntil(promise)
-    e.respondWith(promise)
+        e.respondWith(promise)
+    } catch(err) {
+        console.error("traiterCacheStream Erreur generique", err)
+    }
 }
 
 async function getPartsDownload(fuuid, opts) {
